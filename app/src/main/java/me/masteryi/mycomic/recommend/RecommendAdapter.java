7935398e611139740e2ae3f209bb276013b28175
jon_ly <jon_ly@163.com>
2016-12-10 21:44:39 +0800

    修复cardview阴影不显示问题

--- app/src/main/java/me/masteryi/mycomic/recommend/RecommendAdapter.java
+++ app/src/main/java/me/masteryi/mycomic/recommend/RecommendAdapter.java
@@ -5,7 +5,6 @@ import android.databinding.DataBindingUtil;
 import android.databinding.ViewDataBinding;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
-import android.view.View;
 import android.view.ViewGroup;
 import java.util.ArrayList;
 import java.util.List;
@@ -39,12 +38,6 @@ class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.ItemViewHol
     public void onBindViewHolder (ItemViewHolder holder, int position) {
         ViewDataBinding binding = holder.getBinding();
         binding.setVariable(BR.recommendComicList, mRecommendComics.get(position));
-        binding.getRoot().setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick (View view) {
-                //TODO
-            }
-        });
         binding.executePendingBindings();
     }
 
@@ -69,6 +62,12 @@ class RecommendAdapter extends RecyclerView.Adapter<RecommendAdapter.ItemViewHol
 
         RecommendListItemBinding getBinding () {
             return mBinding;
+        }
+    }
+
+    class Handler {
+        public void onItemClick (String url) {
+            // TODO: 2016/12/10
         }
     }
 }
